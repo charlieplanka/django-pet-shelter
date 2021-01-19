b8b16d06e7beb14acc8c43d5635431332db24c77
Anastasia Naumenko <charlieplanka@gmail.com>
2021-01-19 16:14:36 +0300

    Add contacts page

--- pet_shelter/urls.py
+++ pet_shelter/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import pets, PetDetailView
+from .views import pets, PetDetailView, ContactsView
 from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
     path('', pets, name='pets'),
     path('pets/<str:pk>', PetDetailView.as_view()),
+    path('contacts', ContactsView.as_view(), name='contacts')
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- pet_shelter/views.py
+++ pet_shelter/views.py
@@ -1,12 +1,15 @@
 from pet_shelter.models import Pet
 from django.template import loader
 from django.http import HttpResponse
-from django.views.generic import DetailView
+from django.views.generic import DetailView, TemplateView
 
 
 class PetDetailView(DetailView):
-
     model = Pet
+
+
+class ContactsView(TemplateView):
+    template_name = 'contacts.html'
 
 
 def pets(request):
@@ -14,3 +17,5 @@ def pets(request):
     pets = Pet.objects.order_by('-arrival_date').all()
     pets_data = {'pets': pets}
     return HttpResponse(template.render(pets_data, request))
+
+

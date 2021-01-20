6473ca4a2fb6f1c0989a398016d9316a2ddab01d
Anastasia Naumenko <charlieplanka@gmail.com>
2021-01-20 14:23:01 +0300

    Add specie filter

--- pet_shelter/views.py
+++ pet_shelter/views.py
@@ -12,3 +12,17 @@ class ContactsView(TemplateView):
 
 class PetsListView(ListView):
     model = Pet
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        get_params = self.request.GET.dict()
+
+        # filter
+        if get_params.get('filter'):
+            qs = qs.filter(specie__title=get_params.get('filter'))
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['all_pets'] = Pet.objects.all()
+        return context

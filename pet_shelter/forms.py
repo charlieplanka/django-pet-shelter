0536b79557c200d6c32f965839e2de9fdb7108af
Anastasia Naumenko <charlieplanka@gmail.com>
2021-01-25 15:45:43 +0300

    Add labels in form

--- pet_shelter/forms.py
+++ pet_shelter/forms.py
@@ -7,3 +7,12 @@ class PetForm(forms.ModelForm):
     class Meta:
         model = Pet
         fields = "__all__"
+        labels = {
+            "specie": "Вид питомца",
+            "name": "Имя",
+            "age": "Возраст",
+            "gender": "Пол",
+            "description": "Описание",
+            "breed": "Порода",
+            "photo": "Фото",
+        }
